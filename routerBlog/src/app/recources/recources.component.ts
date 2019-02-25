18b6206ecc6f93bc761193f87fe7066940c03b93
Igor Popach <igorpopach@step.itstep.rv.ua>
2019-02-25 20:58:35 +0200

    router

---
+++ routerBlog/src/app/recources/recources.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recources',
+  templateUrl: './recources.component.html',
+  styleUrls: ['./recources.component.css']
+})
+export class RecourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
